68135b0aaae928a5f78aca526cf9cf8125b6b387
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-03-25 21:30:27 +0600

    fake guns data and folder structure added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+
+const Header = () => {
+    return (
+        <nav className={styles.navbar}>
+            <h1>Guns Store - Online shopping</h1>
+        </nav>
+    );
+};
+
+export default Header;
